d91653520d3639a63a2ccc2fd017e73af3d0975e
Andy Walpole <me@andywalpole.me>
2016-11-11 16:50:55 +0000

    added redux-thunk to app

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+
+
+export function signinUser({email, password}) {
+
+	return function(dispatch) {
+
+		dispatch();
+
+	};
+
+	// submit email/password to the server
+
+
+
+	// if request is good...
+	// - Update state to indicate user is authenticated
+	// - Save the JWT token
+	// - redirect to the route './feature'
+
+	// if request is bad
+
+
+}
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -3,17 +3,30 @@ import {reduxForm} from 'redux-form';
 
 class Signin extends Component {
 
+	constructor() {
+		super(...arguments);
+		this.handleFormSubmit = this.handleFormSubmit.bind(this);
+	}
+
+	handleFormSubmit({email, password}) {
+
+		console.log(email, password);
+
+	}
+
 	render() {
 
+		const {handleSubmit, fields: {email, password}} = this.props;
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.handleFormSubmit)}>
 				<fieldset className="form-group">
 
 					<label htmlFor="emailSignin">Email:</label><br />
-					<input type="email" id="emailSignin" className="form-control"/>
+					<input type="email" id="emailSignin" className="form-control" {...email}/>
 
 					<label htmlFor="passwordSignin">Password:</label><br />
-					<input type="password" id="passwordSignin" className="form-control"/>
+					<input type="password" id="passwordSignin" className="form-control" {...password}/>
 
 					<input type="submit" className="btn button-primary" value="Submit"/>
 
