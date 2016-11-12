3290edaeca69164c6d53da6d324b04fe783dab44
Andy Walpole <me@andywalpole.me>
2016-11-12 15:18:03 +0000

    added react history to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {browserHistory} from 'react-router';
 
 const ROOT_URL = `http://localhost:3090`;
 
@@ -6,18 +7,22 @@ export function signinUser({email, password}) {
 
 	return function(dispatch) {
 
-		axios.post(`${ROOT_URL}/signin`, {email, password});
-
 		// submit email/password to the server
 
+		axios.post(`${ROOT_URL}/signin`, {email, password}).then(response => {
 
-		// if request is good...
-		// - Update state to indicate user is authenticated
-		// - Save the JWT token
-		// - redirect to the route './feature'
+			// if request is good...
+			// - Update state to indicate user is authenticated
+			// - Save the JWT token
+			// - redirect to the route './feature'
+			browserHistory.push('/feature');
 
-		// if request is bad
+		}).catch(() => {
 
+			// if request is bad
+
+
+		});
 
 	};
 
