2da95eb2ea58a5d5370f0ade91433943f9095843
Andy Walpole <me@andywalpole.me>
2016-11-12 19:07:02 +0000

    refactored action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,9 +8,9 @@ import {
 
 const ROOT_URL = `http://localhost:3090`;
 
-export function signinUser({email, password}) {
+class ErrorMesage {
 
-	return function(dispatch) {
+	static displayMessage({email, password}, dispatch) {
 
 		// submit email/password to the server
 
@@ -30,6 +30,16 @@ export function signinUser({email, password}) {
 			dispatch(authError(response.message));
 
 		});
+
+	}
+
+}
+
+export function signinUser({email, password}) {
+
+	return function(dispatch) {
+
+		ErrorMesage.displayMessage({email, password}, dispatch);
 
 	};
 
@@ -39,28 +49,9 @@ export function signupUser({email, password}) {
 
 	return function(dispatch) {
 
-		// submit email/password to the server
-
-		axios.post(`${ROOT_URL}/signin`, {email, password}).then(response => {
-
-			// if request is good...
-			// - Update state to indicate user is authenticated
-			dispatch({type: AUTH_USER});
-			// - Save the JWT token
-			localStorage.setItem('token', response.data.token);
-			// - redirect to the route './feature'
-			browserHistory.push('/feature');
-
-		}).catch((response) => {
-
-			// if request is bad
-			dispatch(authError(response.message));
-
-		});
+		ErrorMesage.displayMessage({email, password}, dispatch);
 
 	};
-
-
 
 }
 
