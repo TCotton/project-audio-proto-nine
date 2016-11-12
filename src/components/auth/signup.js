571415e27dbf5c85ba2fafdee9ab95559a9e81b6
Andy Walpole <me@andywalpole.me>
2016-11-12 17:09:34 +0000

    added signUp smart component

---
+++ src/components/auth/signup.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+
+	render() {
+		return (
+			<div>Sign up here</div>
+		);
+	}
+
+}
+
+export default Signup;
+
+
+
