1911f0b5394c65eaeb2ec31b48d6da20e89269d3
Andy Walpole <me@andywalpole.me>
2016-11-12 18:07:21 +0000

    added password validation

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -4,26 +4,39 @@ import * as actions from '../../actions';
 
 class Signup extends Component {
 
+	passwordErrorDisplay() {
+
+		if (this.props.fields.password.dirty && this.props.fields.passwordConfirm.dirty) {
+			return [
+				`${this.props.fields.password.error}`
+			]
+		}
+
+	}
+
 	render() {
 
 		const {handleSubmit, fields: {email, password, passwordConfirm}} = this.props;
 
 		return (
-			<form>
+			<form onSubmit={handleSubmit}>
 
 				<fieldset className="form-group">
 					<label htmlFor="emailSignup">Email:</label>
-					<input className="form-control" id="emailSignup" {...email}/>
+					<input type="text" className="form-control" id="emailSignup" {...email}/>
 				</fieldset>
 
 				<fieldset className="form-group">
 					<label htmlFor="passwordSignup">Password:</label>
-					<input className="form-control" id="passwordSignup" {...password}/>
+					<input type="text" className="form-control" id="passwordSignup" {...password}/>
+
+					<span>{this.passwordErrorDisplay()}</span>
+
 				</fieldset>
 
 				<fieldset className="form-group">
 					<label htmlFor="passwordSignupConfirm">Confirm password:</label>
-					<input className="form-control" id="passwordSignupConfirm" {...passwordConfirm}/>
+					<input type="text" className="form-control" id="passwordSignupConfirm" {...passwordConfirm}/>
 				</fieldset>
 
 				<input type="submit" className="btn btn-primary" value="Submit"/>
@@ -34,9 +47,25 @@ class Signup extends Component {
 
 }
 
+function validate(fornProps) {
+
+	const errors = {};
+
+	if (!Object.is(fornProps.password, fornProps.passwordConfirm)) {
+		errors.password = 'Passwords must match';
+	}
+
+	if (typeof fornProps.password !== 'undefined' && fornProps.password.length < 9) {
+		errors.password = 'The password must be more than 8 characters';
+	}
+
+	return errors;
+}
+
 export default reduxForm({
 	form: 'signup',
-	fields: ['email', 'password', 'passwordConfirm']
+	fields: ['email', 'password', 'passwordConfirm'],
+	validate
 })(Signup);
 
 
